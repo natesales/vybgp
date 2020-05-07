e084db04db9d6cd3f9ede2357011213783014cb1
Nate Sales <nate@nate.to>
2020-05-07 11:11:21 -0700

    Create vyos object

--- server.py
+++ server.py
@@ -1,37 +1,36 @@
 from flask import Flask, request, render_template, redirect, make_response, Markup
 import requests
-import vymgmt
+from vyos import Router
 
-vyos = vymgmt.Router('10.0.10.2', 'vyos', password='vyos', port=22)
-
-router = "http://10.0.10.2:8000"
+asn = 34553
+vyos = Router("10.0.10.2")
 
 app = Flask(__name__)
 
 @app.route("/")
 def route_index():
-    print("Running IP summary...")
-    output = requests.get(router + "/show ip bgp summary").text.split("\n\n")[1].strip()
-    print("DONE")
+    try:
+        output = vyos.op("show ip bgp summary").split("\n\n")[1].strip()
+    except IndexError:
+        peers_html = ""
+    else:
+        peers_html = ""
+        for peer in output.split("\n")[1:]:
+            elements = peer.split(" ")
+            while "" in elements:
+                elements.remove("")
 
-    peers_html = ""
-    for peer in output.split("\n")[1:]:
-        elements = peer.split(" ")
-        while "" in elements:
-            elements.remove("")
-
-        peers_html += """<tr>
-                            <td class="align-middle"><a class="link-normal" target="_blank" href="https://bgp.tools/asn/""" + elements[2] + """">AS""" + elements[2] + """</a></td>
-                            <td class="align-middle"><a class='link-normal' target='_blank'><i>Unknown</i></a></td>
-                            <td class="align-middle">""" + elements[0] + """</td>
-                            <td class="align-middle">""" + elements[-1] + """</td>
-                            <td class="align-middle">""" + elements[-2] + """</td>
-                            <td class="align-middle pt-3">
-                                <button class="btn btn-danger m-1">Restart</button>
-                                <button class="btn btn-danger m-1">Down</button>
-                            </td>
-                        </tr>
-                        """
+            peers_html += """<tr>
+                                <td class="align-middle"><a class="link-normal" target="_blank" href="https://bgp.tools/asn/""" + elements[2] + """">AS""" + elements[2] + """</a></td>
+                                <td class="align-middle"><a class='link-normal' target='_blank'><i>Unknown</i></a></td>
+                                <td class="align-middle">""" + elements[0] + """</td>
+                                <td class="align-middle">""" + elements[-1] + """</td>
+                                <td class="align-middle">""" + elements[-2] + """</td>
+                                <td class="align-middle pt-3">
+                                    <button class="btn btn-danger m-1" onclick="window.location = '/delete?peer=""" + elements[0] + """'">Delete</button>
+                                </td>
+                            </tr>
+                            """
 
     return render_template("dashboard.html", peers=Markup(peers_html))
 
@@ -40,26 +39,23 @@ def route_index():
 @app.route("/peer", methods=["POST"])
 def route_peer():
     try:
-        asn = request.form["asn"]
+        _asn = request.form["asn"]
         name = request.form["name"]
         address = request.form["address"]
-        group = request.form["group"]
         max_ipv4 = request.form["max-prefixes-ipv4"]
         max_ipv6 = request.form["max-prefixes-ipv6"]
         filter = request.form["filter"]
     except KeyError:
         return redirect("/")
     else:
-        vyos.login()
-        vyos.configure()
-        vyos.set("protocols bgp 1 neighbor " + address + " remote-as " + str(asn))
-        vyos.set("protocols bgp 1 peer-group " + group)
-        vyos.set("protocols bgp 1 neighbor " + address + " peer-group " + group)
-        vyos.commit()
-        vyos.save()
-        vyos.exit()
-        vyos.logout()
+        vyos.set("protocols bgp " + str(asn) + " neighbor " + address + " remote-as " + str(_asn))
 
         return redirect("/")
+
+@app.route("/delete")
+def delete():
+    vyos.delete("protocols bgp " + str(asn) + " neighbor " + request.args.get("peer"))
+
+    return redirect("/")
 
 app.run("localhost", port=8000, debug=True)
