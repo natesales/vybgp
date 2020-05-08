951983a2e15558f5472696b0303f8e16cdac6630
Nate Sales <nate@nate.to>
2020-05-07 19:04:46 -0700

    Reformat

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, render_template, redirect, make_response, Markup
-import requests
-from vyos import Router
 import ipaddress
+from flask import Flask, request, render_template, redirect, Markup
+
+from vyos import Router
 
 asn = 34553
 vyos = Router("10.0.10.2")
 
 app = Flask(__name__)
 
+
 def validate(ip):
-	try:
-		ip = ipaddress.ip_address(ip)
-	except ValueError:
-		return False
-	else:
-		return str(ip)
+    try:
+        ip = ipaddress.ip_address(ip)
+    except ValueError:
+        return False
+    else:
+        return str(ip)
+
 
 @app.route("/")
 def route_index():
@@ -30,14 +32,16 @@ def route_index():
                 elements.remove("")
 
             peers_html += """<tr>
-                                <td class="align-middle"><a class="link-normal" target="_blank" href="https://bgp.tools/asn/""" + elements[2] + """">AS""" + elements[2] + """</a></td>
+                                <td class="align-middle"><a class="link-normal" target="_blank" href="https://bgp.tools/asn/""" + \
+                          elements[2] + """">AS""" + elements[2] + """</a></td>
                                 <td class="align-middle"><a class='link-normal' target='_blank'><i>Unknown</i></a></td>
                                 <td class="align-middle">""" + elements[0] + """</td>
 								<td class="align-middle">Internal</td>
                                 <td class="align-middle">""" + elements[-1] + """</td>
                                 <td class="align-middle">""" + elements[-2] + """</td>
                                 <td class="align-middle pt-3">
-                                    <button class="btn btn-danger m-1" onclick="window.location = '/delete?peer=""" + elements[0] + """'">Delete</button>
+                                    <button class="btn btn-danger m-1" onclick="window.location = '/delete?peer=""" + \
+                          elements[0] + """'">Delete</button>
                                 </td>
                             </tr>
                             """
@@ -58,8 +62,9 @@ def route_peer():
     except KeyError:
         return redirect("/")
     else:
-    	vyos.set("protocols bgp " + str(asn) + " neighbor " + address + " remote-as " + str(_asn))
-		return redirect("/")
+        vyos.set("protocols bgp " + str(asn) + " neighbor " + address + " remote-as " + str(_asn))
+        return redirect("/")
+
 
 @app.route("/delete")
 def delete():
@@ -67,43 +72,44 @@ def delete():
 
     return redirect("/")
 
+
 @app.route("/lg", methods=["GET", "POST"])
 def index():
-	if request.method == "GET":
-		return render_template("lg.html")
-	elif request.method == "POST":
-		command = request.form["command"]
-		arg = request.form["arg"]
+    if request.method == "GET":
+        return render_template("lg.html")
+    elif request.method == "POST":
+        command = request.form["command"]
+        arg = request.form["arg"]
 
-		if command == "ping":
-			if validate(arg):
-				output = vyos.op("ping " + arg + " count 1")
-			else:
-				output = "Invalid query."
+        if command == "ping":
+            if validate(arg):
+                output = vyos.op("ping " + arg + " count 1")
+            else:
+                output = "Invalid query."
 
-			return render_template("lg.html", output=output)
+            return render_template("lg.html", output=output)
 
-		elif command == "traceroute":
-			if validate(arg):
-				output = vyos.op("traceroute " + arg)
-			else:
-				output = "Invalid query."
+        elif command == "traceroute":
+            if validate(arg):
+                output = vyos.op("traceroute " + arg)
+            else:
+                output = "Invalid query."
 
-			return render_template("lg.html", output=output)
+            return render_template("lg.html", output=output)
 
-		elif command == "show-ip-bgp-neighbor":
-			if validate(arg):
-				output = vyos.op("show ip bgp neighbor " + arg)
-			else:
-				output = "Invalid query."
+        elif command == "show-ip-bgp-neighbor":
+            if validate(arg):
+                output = vyos.op("show ip bgp neighbor " + arg)
+            else:
+                output = "Invalid query."
 
-			return render_template("lg.html", output=output)
+            return render_template("lg.html", output=output)
 
-		elif command == "show-ip-bgp-summary":
-			return render_template("lg.html", output=vyos.op("show ip bgp summary"))
+        elif command == "show-ip-bgp-summary":
+            return render_template("lg.html", output=vyos.op("show ip bgp summary"))
 
-		else:
-			return render_template("lg.html", output="Please select a command.")
+        else:
+            return render_template("lg.html", output="Please select a command.")
 
 
 app.run("localhost", port=8000, debug=True)
